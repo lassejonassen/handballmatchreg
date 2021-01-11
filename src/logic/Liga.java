a8677248e13d82a890d9cd5bd6f6d67567ec68f1
Lucaselley <Lucas_elley8@live.dk>
2021-01-11 12:50:15 +0100

    Liga

---
+++ src/logic/Liga.java
@@ -0,0 +1,13 @@
+package logic;
+
+import java.util.ArrayList;
+
+public class Liga {
+	
+	
+	
+	ArrayList<Team> teams = new ArrayList<>();
+	
+	
+	
+}
---
+++ src/logic/LigaInterface.java
@@ -0,0 +1,9 @@
+package logic;
+
+public interface LigaInterface {
+	
+	public void create(Liga liga);
+	public void update(Liga liga);
+	public void delete(Liga liga);
+	
+}
