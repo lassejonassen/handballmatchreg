e1f631453e6554ceaaf4693c7859cf68e2542580
Lasse Jonassen <lassemoellerjonassen@gmail.com>
2021-01-15 12:45:24 +0100

    Updated spCreateMath

--- sqlscripts/Stored Procedures/spCreateMatch.sql
+++ sqlscripts/Stored Procedures/spCreateMatch.sql
@@ -1,9 +1,9 @@
 USE [HbmrDb];
 GO
-CREATE PROCEDURE spCreateMatch @team1_id INT, @team2_id INT
+CREATE PROCEDURE spCreateMatch @team1_id INT, @team2_id INT, @league_id INT
 AS
 BEGIN
 	INSERT INTO TMatch
-	VALUES (@team1_id, @team2_id, 0, 0, 0)
+	VALUES (@team1_id, @team2_id, 0, 0, @league_id)
 END
 GO
