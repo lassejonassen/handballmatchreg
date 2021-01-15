92a0af93987a1928942529ff4b0543b24e8312e6
Lasse Jonassen <lassemoellerjonassen@gmail.com>
2021-01-15 09:38:41 +0100

    Updated spUpdateTeam

--- sqlscripts/Stored Procedures/spUpdateTeam.sql
+++ sqlscripts/Stored Procedures/spUpdateTeam.sql
@@ -3,7 +3,7 @@ GO
 CREATE PROCEDURE spUpdateTeam @team_id INT, @nTeam_name NVARCHAR(20), @league_id INT
 AS
 BEGIN
-	UPDATE Team SET team_name = @nTeam_name
-	WHERE id = @team_id and league_id = @league_id;
+	UPDATE Team SET team_name = @nTeam_name, league_id = @league_id
+	WHERE id = @team_id
 END
 GO
