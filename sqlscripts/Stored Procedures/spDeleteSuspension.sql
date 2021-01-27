f95770b4af8d996fc1439af16746459e9f4bfe4c
Lucaselley <Lucas_elley8@live.dk>
2021-01-27 11:48:24 +0100

    spDeleteSuspension

--- sqlscripts/Stored Procedures/spDeleteSuspension.sql
+++ sqlscripts/Stored Procedures/spDeleteSuspension.sql
@@ -1,9 +1,10 @@
 USE [HbmrDb];
 GO
-CREATE PROCEDURE spDeleteSuspension @match_id INT, @team_id INT, @suspension_id INT
+CREATE PROCEDURE spDeleteSuspension @match_id INT, @team_id INT
 AS
 BEGIN
-	DELETE FROM Suspension
-	WHERE id = @suspension_id
+	DECLARE @id INT;
+	SET @id = (SELECT IDENT_CURRENT('Suspension') FROM Suspension WHERE @match_id = Suspension.match_id AND @team_id = Suspension.team_id);
+	DELETE FROM Suspension WHERE Suspension.id = @id;
 END
 GO
